b79813ed9f38ace96d919580ca77746c83828d81
Pedronex <pedrosoares.nex@gmail.com>
2021-12-14 18:01:42 -0300

    atualização de informativo

--- assets/scripts/painel.js
+++ assets/scripts/painel.js
@@ -1,20 +1,23 @@
 window.addEventListener('storage', function () {
-  this.document.getElementById('sorteio').innerHTML = ''
   const historico = localStorage.getItem('historico') ? JSON.parse(localStorage.getItem('historico')) : [];
   const apresentar = localStorage.getItem('apresentar');
   let estrutura = ''
   console.log(apresentar)
-  const sorteados = historico[historico.length - 1].listaSorteados;
-  estrutura += "<div class='container'>"
-  estrutura += '<article class="componente">'
-  sorteados.forEach((item, index) => {
-    estrutura += '<article class="conteudo">'
-    estrutura += `<span class="numero">${item.brinde}</span>`
-    estrutura += `<span class="label" style="${apresentar == 'true' ? 'opacity: 1;' : 'opacity: 0;'}" >N°</span>`
-    estrutura += `<span class="numero" style="${apresentar == 'true' ? 'opacity: 1;' : 'opacity: 0;'}">${item.numero < 10 ? '0' + item.numero : item.numero}</span>`
+  console.log(historico)
+  if (historico.length != 0) {
+    const sorteados = historico[historico.length - 1].listaSorteados;
+    this.document.getElementById('sorteio').innerHTML = estrutura
+    estrutura += "<div class='container'>"
+    estrutura += '<article class="componente">'
+    sorteados.forEach((item, index) => {
+      estrutura += '<article class="conteudo">'
+      estrutura += `<span class="numero">${item.brinde}</span>`
+      estrutura += `<span class="label" style="${apresentar == 'true' ? 'opacity: 1;' : 'opacity: 0;'}" >N°</span>`
+      estrutura += `<span class="numero" style="${apresentar == 'true' ? 'opacity: 1;' : 'opacity: 0;'}">${item.numero < 10 ? '0' + item.numero : item.numero}</span>`
+      estrutura += '</article>'
+    });
     estrutura += '</article>'
-  });
-  estrutura += '</article>'
-  estrutura += '</div>'
-  this.document.getElementById('sorteio').innerHTML = estrutura
+    estrutura += '</div>'
+    this.document.getElementById('sorteio').innerHTML = estrutura
+  }
 })
