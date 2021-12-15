7ed625a6d004c0fee3055f57ebdebda3fc191e93
Pedronex <pedrosoares.nex@gmail.com>
2021-12-15 11:14:04 -0300

    atualização de ultima hora

--- assets/scripts/painel.js
+++ assets/scripts/painel.js
@@ -12,12 +12,29 @@ window.addEventListener('storage', function () {
     sorteados.forEach((item, index) => {
       estrutura += '<article class="conteudo">'
       estrutura += `<span class="numero">${item.brinde}</span>`
-      estrutura += `<span class="label" style="${apresentar == 'true' ? 'opacity: 1;' : 'opacity: 0;'}" >N°</span>`
-      estrutura += `<span class="numero" style="${apresentar == 'true' ? 'opacity: 1;' : 'opacity: 0;'}">${item.numero < 10 ? '0' + item.numero : item.numero}</span>`
+      estrutura += `<span class="label" id="mostrarL">N°</span>`
+      estrutura += `<span class="numero1" id="mostrarN">${item.numero < 10 ? '0' + item.numero : item.numero}</span>`
       estrutura += '</article>'
     });
     estrutura += '</article>'
     estrutura += '</div>'
     this.document.getElementById('sorteio').innerHTML = estrutura
+  }
+})
+
+window.addEventListener('storage', function () {
+  const apresentar = localStorage.getItem('apresentar');
+  const historico = localStorage.getItem('historico') ? JSON.parse(localStorage.getItem('historico')) : [];
+
+  const participantes = historico[historico.length - 1].listaParticipantes;
+  console.log(participantes)
+  if (apresentar == 'true') {
+    for (i = this.document.getElementsByClassName('numero1').length; i--;) {
+      this.document.getElementsByClassName('numero1').item(i).className = 'numero-dado';
+    }
+  } else {
+    for (i = this.document.getElementsByClassName('numero1').length; i--;) {
+      this.document.getElementsByClassName('numero1').item(i).innerHTML = "<img src='../../assets/img/numeros.gif' alt='numeros aleatórios' width='200'/>"
+    }
   }
 })
